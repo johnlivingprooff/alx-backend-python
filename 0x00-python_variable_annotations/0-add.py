19934669253f35e7ba2cfa690bd58b86f4c2d1c0
johnlivingprooff <johnlivingprooff@gmail.com>
2024-05-02 18:57:29 +0200

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Type-annotated function add that takes a float a and a float b as arguments"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floats"""
+    return a + b
