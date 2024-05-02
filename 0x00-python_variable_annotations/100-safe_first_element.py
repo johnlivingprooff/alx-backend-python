a6bd98f57ccea32c8295cef610a2933c901e2cc2
johnlivingprooff <johnlivingprooff@gmail.com>
2024-05-02 19:58:40 +0200

    updatess

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """duck-typed annotations add"""
-from typing import Union, Any
+from typing import Union
+from typing import Any
+from typing import Sequence
 
 
-def safe_first_element(lst: list) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """returns first element of a list if exists, otherwise None"""
     if lst:
         return lst[0]
