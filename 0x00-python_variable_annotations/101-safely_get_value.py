d61fc610336931f9c19ed8eb76d4a9d0d89661e8
johnlivingprooff <johnlivingprooff@gmail.com>
2024-05-02 20:05:37 +0200

    101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """TypeVar module with annotations"""
-from typing import Dict
+from typing import Dict, Any
 from typing import TypeVar
-from typing import Optional
-
-KT = TypeVar('KT')
-VT = TypeVar('VT')
+from typing import Union
+from typing import Mapping
 
 
-def safely_get_value(dct: Dict[KT, VT], key: KT,
-                     default: Optional[VT] = None) -> VT:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[TypeVar('T'), None] = None
+                     ) -> Union[Any, TypeVar("T")]:
     """Return a value of a key in a dictionary"""
     if key in dct:
         return dct[key]
