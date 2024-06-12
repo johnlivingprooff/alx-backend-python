e057876f0c75c4df18afa0739b05ea3f1bc56595
johnlivingprooff <johnlivingprooff@gmail.com>
2024-06-12 14:54:42 +0200

    Iterable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,8 +3,9 @@
 from typing import List
 from typing import Tuple
 from typing import Sequence
+from typing import Iterable
 
 
-def element_length(lst: List[str]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns a tuple with the length of a string and a list of lengths"""
     return (len(lst), [len(i) for i in lst])
